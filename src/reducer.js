baba53cefda1284950994ccf47f51ed7a06bdf19
Amos Bafuna <amosbafuna24@gmail.com>
2022-08-24 14:50:16 +0100

    Add: Vercl.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
 	playlists: [],
 	playing: false,
 	item: null,
-	/* token:
+	/* 	token:
 		"BQAncALq5xezxLkbfTLnEBZbfAWhotIYTPwBnRU6bk_WM7EMTPrudHaw0iDBtIBYU8d8nD4bxqJo6Tjy0HcvdjRgbQx4a30R5Rc89Jf3Ol3JQ9rR44fIyy-sXvDlSkWnynZqgjpXtv3_u960iG8HIiV2gPq-ft9WDzB7G9GBXjrVPAHcZ5KWTjXpshkV5Cxnf9OkCbqljY8yqVXeP7LPKeAxxug365rKHdmalTU55Nv_uJ-uXsjFdIwqM07ANR8Rve22S7JzAElzosMWAf2_dPht7EmqCZaotO5p98H8YK8L-YbO-09G__CVb88_LwC3F_9kVg",
  */
 };

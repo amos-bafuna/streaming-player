c6c855de16d65abdb9841301567034a70e8bf137
Amos Bafuna <amosbafuna24@gmail.com>
2022-09-01 16:46:35 +0100

    Feat: manage responsive device

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "../src/images/PlayerLogo.png";
 import { useDataLayerValue } from "./DataLayer";
 import SidebarOption from "./SidebarOption";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import LoyaltyRoundedIcon from "@mui/icons-material/LoyaltyRounded";
+import { AiOutlineMenu } from "react-icons/ai";
+import { CgCloseO } from "react-icons/cg";
 import "./Sidebar.css";
+import { useState } from "react";
 
 function Sidebar() {
+	const [open, setOpen] = useState(false);
+	const [isMobile, setIsMobile] = useState(false);
 	const [{ playlists, user }] = useDataLayerValue();
+
+	const onWindowResize = (event) => {
+		setIsMobile(window.innerWidth < 915);
+	};
+
+	useEffect(() => {
+		setIsMobile(window.innerWidth < 915);
+		window.addEventListener("resize", onWindowResize);
+		return () => {
+			window.removeEventListener("resize", onWindowResize);
+		};
+	}, []);
+
 	return (
 		<div className="sidebar_container">
-			<div className="sidebar">
+			<div
+				className="sidebar"
+				style={
+					isMobile ? { display: open ? "block" : "flex" } : { display: "block" }
+				}
+			>
 				<img src={Logo} alt="Logo" className="sidebar_logo" />
 
-				<SidebarOption Icon={HomeIcon} title="Home" link="/" key={"a"} />
+				<SidebarOption
+					open={open}
+					isMobile={isMobile}
+					Icon={HomeIcon}
+					title="Home"
+					link="/"
+					key={"a"}
+				/>
 				<SidebarOption
 					Icon={SearchIcon}
+					open={open}
+					isMobile={isMobile}
 					title="Search"
 					link="/search"
 					key={"b"}
 				/>
 				<SidebarOption
 					Icon={LoyaltyRoundedIcon}
+					open={open}
+					isMobile={isMobile}
 					title="Favorite"
 					link="/favorite"
 					key={"c"}
 				/>
+				<div className="hambourger_menu">
+					{open ? (
+						<CgCloseO onClick={() => setOpen(false)} />
+					) : (
+						<AiOutlineMenu onClick={() => setOpen(true)} />
+					)}
+				</div>
 
 				<br />
 				<strong className="sidebarTitle">Playlist</strong>
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./SidebarOption.css";
 import { NavLink } from "react-router-dom";
 
-function SidebarOption({ title, Icon, link, cle }) {
+function SidebarOption({ title, Icon, link, cle, open, isMobile }) {
 	return (
 		<div key={cle}>
 			<NavLink
 				to={link ?? ""}
 				className={({ isActive }) =>
 					isActive ? "is-active sidebarOption" : "sidebarOption"
+				}
+				style={
+					isMobile ? { display: open ? "flex" : "none" } : { display: "flex" }
 				}
 			>
 				{Icon && <Icon className="sidebarOptionIcon" />}
